e0e784355606261503799ede2a538e442fccb915
quyle <huuquyle9@gmail.com>
2024-11-16 00:46:15 +0700

    041

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,31 +1,35 @@
-const connection = require("../config/database")
+const connection = require("../config/database");
+const { getAllUser } = require("../services/CRUDservice");
 
 
-const getHomePage = (req, res) => {
-    return res.render('home.ejs')
+const getHomePage = async (req, res) => {
+    const results = await getAllUser()
+    return res.render('home.ejs', {
+        listUsers : results
+    })
 }
 
 const getQuyIT = (req, res) => {
     res.render('sample.ejs')
 }
 
-const postCreateNewUser = (req, res) => {
+const getCreate = (req, res) => {
+    res.render('create.ejs')
+}
+
+const postCreateNewUser = async (req, res) => {
     console.log(req.body)
     const { email, name, city } = req.body
-    connection.query(
-        `INSERT INTO Users(email, name, city) VALUES(?,?,?)`,
-        [email, name, city],
-        function(err, result){
-            console.log(result)
-            res.send("create new user success")
-        }
+    const [results, fields] = await connection.query(
+        `INSERT INTO Users(email, name, city) VALUES(?,?,?)`,[email, name, city]
     )
-    // res.send("create new user")
+    res.send("create new user")
 
 }
 
 module.exports = {
     getHomePage,
     getQuyIT,
-    postCreateNewUser
+    postCreateNewUser,
+    getCreate
 }
--- src/routes/web.js
+++ src/routes/web.js
@@ -1,10 +1,12 @@
-const {getHomePage, getQuyIT, postCreateNewUser} = require("../controllers/homeController")
+const {getHomePage, getQuyIT, postCreateNewUser, getCreate} = require("../controllers/homeController")
 const express = require("express")
 const router = express.Router();
 
 router.get('/', getHomePage)
 
 router.get('/quyit', getQuyIT)
+
+router.get('/create', getCreate)
 
 router.post('/create-user', postCreateNewUser)
 
